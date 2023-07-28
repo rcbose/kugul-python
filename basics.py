6f6c192bdc9131043fa398525bdd704366bc51bf
kogul <kogul121@gmail.com>
2023-07-28 21:48:47 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,7 @@
+print ( 'hi' )
+fruits = ['apple', 'banana', 'orange', 'grape']
+
+print(fruits[0])  # Output: 'apple'
+print(fruits[2])  # Output: 'orange'
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
