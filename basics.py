95f77e102de8a3ba84c5767dab360593e5ec0e84
kogul <kogul121@gmail.com>
2023-07-30 22:42:14 +0530

    30_07_23

--- basics.py
+++ basics.py
@@ -1,7 +1,35 @@
 print ( 'hi' )
 fruits = ['apple', 'banana', 'orange', 'grape']
 
-print(fruits[0])  # Output: 'apple'
-print(fruits[2])  # Output: 'orange'
+print(fruits[0]) 
+print(fruits[2]) 
 numbers = [1, 2, 3, 4, 5]
 print(numbers)
+a = 20
+b = a
+print (a)
+numbers = list(range(1, 11))
+print(numbers)
+
+keys = ['name', 'age', 'city']
+values = ['kogul', 21, 'arupukottai']
+
+data = dict(zip(keys, values))
+print(data)
+
+fruits_tuple = ('apple', 'banana', 'orange')
+
+print(fruits_tuple[0]) 
+print(fruits_tuple[2])  
+fruits_tuple = ('apple', 'banana', 'orange')
+
+fruit1, fruit2, fruit3 = fruits_tuple
+
+print(fruit1) 
+print(fruit2)  
+print(fruit3)  
+
+fruits_tuple = ('banana', 'orange')
+
+length = len(fruits_tuple)
+print(length) 
