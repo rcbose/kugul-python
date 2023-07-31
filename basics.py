6dfffe335b5a1ce57ec1679d456a722c51f86049
kogul <kogul121@gmail.com>
2023-07-31 19:00:03 +0530

    31_07_23

--- basics.py
+++ basics.py
@@ -33,3 +33,42 @@ fruits_tuple = ('banana', 'orange')
 
 length = len(fruits_tuple)
 print(length) 
+# Swap variables using a temporary variable
+a = 10
+b = 20
+
+# Swapping values
+temp = a
+a = b
+b = temp
+
+print("a:", a)  # Output: a: 20
+print("b:", b)  # Output: b: 10
+# Swap variables without using a temporary variable or tuple unpacking (using bitwise XOR)
+x = 10
+y = 20
+
+# Swapping values
+x = x ^ y
+y = x ^ y
+x = x ^ y
+
+print("x:", x)  # Output: x: 20
+print("y:", y) 
+
+a = 10 
+b = 5   
+result_and = a & b
+print("Bitwise AND:", result_and)  
+
+result_or = a | b
+print("Bitwise OR:", result_or)   
+
+result_xor = a ^ b
+print("Bitwise XOR:", result_xor) 
+
+result_left_shift = a << 2
+print("Left Shift of a by 2:", result_left_shift)
+
+result_right_shift = a >> 2
+print("Right Shift of a by 2:", result_right_shift) 
