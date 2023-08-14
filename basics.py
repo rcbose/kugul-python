9023447ff076719741f3a829beaeaf4405cf272f
kogul <kogul121@gmail.com>
2023-08-15 00:22:00 +0530

    if

--- basics.py
+++ basics.py
@@ -116,3 +116,18 @@ result = add_numbers(num1, num2)
 
 print("The sum of", num1, "and", num2, "is:", result)
 
+age = 15
+if age >= 18:
+    print("You are an eligible")
+else:
+    print("You are a not eligible")
+
+score = 85
+if score >= 90:
+    print("You got an A.")
+elif score >= 80:
+    print("You got a B.")
+elif score >= 70:
+    print("You got a C.")
+else:
+    print("You need to improve.")
